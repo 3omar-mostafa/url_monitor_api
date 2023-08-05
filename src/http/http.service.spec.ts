42cebc347484a849514dbd805e25845d2e6f9990
Omar Mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2023-08-05 22:21:03 +0300

    Update Some unit tests

--- src/http/http.service.spec.ts
+++ src/http/http.service.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HttpService } from './http.service';
+import { HttpModule as Http } from '@nestjs/axios/dist/http.module';
+import { NotificationModule } from '../notification/notification.module';
+import { closeMongoConnection, rootMongooseTestModule } from '../../test/utils/in-memory-mongodb';
 
 describe('HttpService', () => {
   let service: HttpService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [rootMongooseTestModule(), Http, NotificationModule],
       providers: [HttpService],
+      exports: [HttpService],
     }).compile();
 
     service = module.get<HttpService>(HttpService);
@@ -14,5 +19,9 @@ describe('HttpService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  afterEach(async () => {
+    await closeMongoConnection();
   });
 });
--- src/pipes/parse-object-id.pipe.spec.ts
+++ src/pipes/parse-object-id.pipe.spec.ts
@@ -1,7 +1,21 @@
 import { ParseObjectIdPipe } from './parse-object-id.pipe';
+import { BadRequestException } from '@nestjs/common';
 
 describe('ParseObjectIdPipe', () => {
   it('should be defined', () => {
     expect(new ParseObjectIdPipe()).toBeDefined();
+  });
+
+  it('should parse ObjectId', () => {
+    const pipe = new ParseObjectIdPipe();
+    const result = pipe.transform('5a2345678901234567890123');
+    expect(result).toBeDefined();
+  });
+
+  it('should not parse invalid ObjectId', () => {
+    const pipe = new ParseObjectIdPipe();
+    expect(() => {
+      pipe.transform('invalid');
+    }).toThrow(BadRequestException);
   });
 });

bcea4a8d623feea967db7684fa0d851c1d71e8cf
Omar Mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2023-08-05 18:53:08 +0300

    User proper status codes for authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Query, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Query, UseInterceptors } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../models/dto/user/createUserDto';
 import { LoginUserDto } from '../models/dto/user/LoginUserDto';
@@ -12,11 +12,13 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
+  @HttpCode(HttpStatus.OK)
   login(@Body() user: LoginUserDto) {
     return this.authService.logIn(user.email, user.password);
   }
 
   @Post('signup')
+  @HttpCode(HttpStatus.CREATED)
   @UseInterceptors(AuthController.responseTransformInterceptor)
   signup(@Body() user: CreateUserDto): Promise<ReturnUserDto> {
     return this.authService.signup(user);

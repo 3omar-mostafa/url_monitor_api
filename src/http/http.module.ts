82c7d3e9df081d6a0f2e8abba2dd1ada4996d577
Omar Mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2023-08-05 20:18:44 +0300

    Remove Unused imports

--- src/http/http.module.ts
+++ src/http/http.module.ts
@@ -2,12 +2,9 @@ import { Module } from '@nestjs/common';
 import { HttpModule as Http } from '@nestjs/axios';
 import { HttpService } from './http.service';
 import { NotificationModule } from '../notification/notification.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { UrlCheck, UrlCheckSchema } from '../models/UrlCheck';
-import { Models } from '../models/constants';
 
 @Module({
-  imports: [Http, NotificationModule, MongooseModule.forFeature([{ name: Models.URL_CHECK, schema: UrlCheckSchema }])],
+  imports: [Http, NotificationModule],
   providers: [HttpService],
   exports: [HttpService],
 })
